27e573a0565fae4abe2f172c733259f67a7f7ae0
Gabriel Fiuza <gpaulinellifiuza@gmail.com>
2025-06-12 15:24:15 -0300

    added jump on komb

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,6 @@ import './styles/index.css'
 import Game from './components/Game'
 
 export default function App() {
-
-  const kombImg = document.querySelector('.komb');
-
-  const jump = () => {
-      kombImg.classList.add('jump');
-
-      setTimeout(() => {
-          kombImg.classList.remove('jump');
-      }, 500)
-
-  }
-
-  document.addEventListener('keydown', jump);
-
   return (
     <>
       <Header />
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,29 @@
 import './componentIndex.css';
 import komb from '../assets/komb-automic.png';
 import muro from '../assets/muro-tijolo.jpg';
+import { useEffect, useState } from 'react'
 
 export default function Game() {
   
+  const [isJumping, setIsJumping] = useState(false)
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Space' && !isJumping) {
+        setIsJumping(true)
+        setTimeout(() => {
+          setIsJumping(false)
+        }, 500) // Duração do pulo (deve bater com o CSS)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [isJumping])
+
   return (
-    <div className="game">
-      <img className='komb' src={komb} alt="" />
+    <div className='game'>
+      <img className={`komb ${isJumping ? 'jump' : ''}`} src={komb} alt="" />
       <img className='wall' src={muro} alt="" />
     </div>
   )
