f520371f1efc31c3137ebeb62139e3b669d1915e
Gabriel Fiuza <gpaulinellifiuza@gmail.com>
2025-06-14 00:49:04 -0300

    some navs deleted and href created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,8 @@ export default function Header() {
           <li><a href="#">Início</a></li>
           <li><a href="#">Quem Somos</a></li>
           <li><a href="#services">Nossos Serviços</a></li>
-          <li><a href="#">Portfólio</a></li>
-          <li><a href="#">Nossa Equipe</a></li>
           <li><a href="#footer">Contato</a></li>
-          <li><a href="#" id="join-us">Junte-se a Nós</a></li>
+          <li><a href="#footer" id="join-us">Junte-se a Nós</a></li>
         </ul>
       </nav>
     </header>
--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
     <section className="hero-section">
       <h1>Soluções Inovadoras em Engenharia de Controle e Automação</h1>
       <p>Transformando ideias em realidade com a expertise da Automic Jr.</p>
-      <a href="#" className="hero-cta">Conheça Nossos Projetos</a>
+      <a href="#services" className="hero-cta">Conheça Nossos Projetos</a>
     </section>
   )
 }
